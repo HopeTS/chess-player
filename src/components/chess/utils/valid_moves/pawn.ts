674e34103f2d0e4c29fdd33e523ea5e34d7e0165
Robert Bergers <bergersr97@gmail.com>
2021-06-21 10:24:46 -0400

    add enPassant to validMoves when its a valid move

--- src/components/chess/utils/valid_moves/pawn.ts
+++ src/components/chess/utils/valid_moves/pawn.ts
@@ -1,7 +1,9 @@
-import { coord, IChessPieceData, IClientChessState } from "../../../../types";
+import { coord, IChessPieceData, IClientChessState, move } from "../../../../types";
+import { get_piece } from "../get_piece";
 import { has_piece } from "../has_piece";
 import { is_on_board } from "../is_on_board";
 
+/** Get all valid moves of a pawn given the state of the chess board */
 export function pawn(chessState: IClientChessState, piece: IChessPieceData): coord[] {
 	let validMoves: coord[] = [];
 
@@ -26,22 +28,73 @@ export function pawn(chessState: IClientChessState, piece: IChessPieceData): coo
 
 		// Left capture
 		moveCoords = [piece.coords[0] - 1, piece.coords[1] - 1];
-        if (is_on_board(moveCoords)) {
-            hasPiece = has_piece(chessState.black, moveCoords);
-            if (hasPiece) validMoves.push(moveCoords)
-        }
+		if (is_on_board(moveCoords)) {
+			hasPiece = has_piece(chessState.black, moveCoords);
+			if (hasPiece) validMoves.push(moveCoords);
+		}
 
 		// Right capture
 		moveCoords = [piece.coords[0] - 1, piece.coords[1] + 1];
-        if (is_on_board(moveCoords)) {
-            hasPiece = has_piece(chessState.black, moveCoords);
-            if (hasPiece) validMoves.push(moveCoords)
-        }
+		if (is_on_board(moveCoords)) {
+			hasPiece = has_piece(chessState.black, moveCoords);
+			if (hasPiece) validMoves.push(moveCoords);
+		}
+
+        // En passant captures
+		if (piece.coords[0] === 3) {
+			// Left en passant
+			moveCoords = [piece.coords[0] - 1, piece.coords[1] - 1];
+
+			if (is_on_board(moveCoords)) {
+				// Check if enPassantPiece is a pawn
+				const enPassantPiece = get_piece(chessState, [piece.coords[0], piece.coords[1] - 1]);
+				if (enPassantPiece?.piece === 1) {
+					// Check if the pawn double moved last turn
+					const enPassantMove: move = [
+						[enPassantPiece.coords[0] - 2, enPassantPiece.coords[1]],
+						enPassantPiece.coords,
+					];
+					const lastMove = chessState.history[chessState.history.length - 1];
+					if (
+						enPassantMove[0][0] === lastMove[0][0] &&
+						enPassantMove[0][1] === lastMove[0][1] &&
+						enPassantMove[1][0] === lastMove[1][0] &&
+						enPassantMove[1][1] === lastMove[1][1]
+					) {
+						validMoves.push(moveCoords);
+					}
+				}
+			}
+
+			// Right en passant
+			moveCoords = [piece.coords[0] - 1, piece.coords[1] + 1];
+
+			if (is_on_board(moveCoords)) {
+				// Check if enPassantPiece is a pawn
+				const enPassantPiece = get_piece(chessState, [piece.coords[0], piece.coords[1] + 1]);
+				if (enPassantPiece?.piece === 1) {
+					// Check if the pawn double moved last turn
+					const enPassantMove: move = [
+						[enPassantPiece.coords[0] - 2, enPassantPiece.coords[1]],
+						enPassantPiece.coords,
+					];
+					const lastMove = chessState.history[chessState.history.length - 1];
+					if (
+						enPassantMove[0][0] === lastMove[0][0] &&
+						enPassantMove[0][1] === lastMove[0][1] &&
+						enPassantMove[1][0] === lastMove[1][0] &&
+						enPassantMove[1][1] === lastMove[1][1]
+					) {
+						validMoves.push(moveCoords);
+					}
+				}
+			}
+		}
 	}
 
 	// Black
 	else {
-        // Normal move
+		// Normal move
 		let moveCoords: coord = [piece.coords[0] + 1, piece.coords[1]];
 		let hasPiece: boolean = false;
 		if (is_on_board(moveCoords)) {
@@ -60,17 +113,68 @@ export function pawn(chessState: IClientChessState, piece: IChessPieceData): coo
 
 		// Left capture
 		moveCoords = [piece.coords[0] + 1, piece.coords[1] - 1];
-        if (is_on_board(moveCoords)) {
-            hasPiece = has_piece(chessState.white, moveCoords);
-            if (hasPiece) validMoves.push(moveCoords)
-        }
+		if (is_on_board(moveCoords)) {
+			hasPiece = has_piece(chessState.white, moveCoords);
+			if (hasPiece) validMoves.push(moveCoords);
+		}
 
 		// Right capture
 		moveCoords = [piece.coords[0] + 1, piece.coords[1] + 1];
-        if (is_on_board(moveCoords)) {
-            hasPiece = has_piece(chessState.white, moveCoords);
-            if (hasPiece) validMoves.push(moveCoords)
-        }
+		if (is_on_board(moveCoords)) {
+			hasPiece = has_piece(chessState.white, moveCoords);
+			if (hasPiece) validMoves.push(moveCoords);
+		}
+
+        // En passant captures
+		if (piece.coords[0] === 4) {
+			// Left en passant
+			moveCoords = [piece.coords[0] + 1, piece.coords[1] - 1];
+
+			if (is_on_board(moveCoords)) {
+				// Check if enPassantPiece is a pawn
+				const enPassantPiece = get_piece(chessState, [piece.coords[0], piece.coords[1] - 1]);
+				if (enPassantPiece?.piece === 1) {
+					// Check if the pawn double moved last turn
+					const enPassantMove: move = [
+						[enPassantPiece.coords[0] + 2, enPassantPiece.coords[1]],
+						enPassantPiece.coords,
+					];
+					const lastMove = chessState.history[chessState.history.length - 1];
+					if (
+						enPassantMove[0][0] === lastMove[0][0] &&
+						enPassantMove[0][1] === lastMove[0][1] &&
+						enPassantMove[1][0] === lastMove[1][0] &&
+						enPassantMove[1][1] === lastMove[1][1]
+					) {
+						validMoves.push(moveCoords);
+					}
+				}
+			}
+
+			// Right en passant
+			moveCoords = [piece.coords[0] + 1, piece.coords[1] + 1];
+
+			if (is_on_board(moveCoords)) {
+				// Check if enPassantPiece is a pawn
+				const enPassantPiece = get_piece(chessState, [piece.coords[0], piece.coords[1] + 1]);
+				if (enPassantPiece?.piece === 1) {
+					// Check if the pawn double moved last turn
+					const enPassantMove: move = [
+						[enPassantPiece.coords[0] + 2, enPassantPiece.coords[1]],
+						enPassantPiece.coords,
+					];
+					const lastMove = chessState.history[chessState.history.length - 1];
+					if (
+						enPassantMove[0][0] === lastMove[0][0] &&
+						enPassantMove[0][1] === lastMove[0][1] &&
+						enPassantMove[1][0] === lastMove[1][0] &&
+						enPassantMove[1][1] === lastMove[1][1]
+					) {
+						validMoves.push(moveCoords);
+					}
+				}
+			}
+		}
 	}
 	return validMoves;
 }
