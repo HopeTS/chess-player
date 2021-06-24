3bf1ebc3c20460ab74e48b7f04c24a17e6e539a8
Robert Bergers <bergersr97@gmail.com>
2021-06-24 04:04:37 -0400

    generally useful utils functions

---
+++ src/components/chess/utils/pieces/pieces.ts
@@ -0,0 +1,4 @@
+export * from "./get_black_pieces";
+export * from "./get_white_pieces";
+export * from "./has_piece";
+export * from "./get_piece";
--- src/components/chess/utils/utils.ts
+++ src/components/chess/utils/utils.ts
@@ -1,6 +1,8 @@
 export * as valid_moves from "./valid_moves/valid_moves";
-export * as in_check from "./in_check/in_check";
+export * as in_check from "./check/get_check_status";
 export * as possible_paths from "./possible_paths/possible_paths";
+export * as check from "./check/check";
+export * as pieces from "./pieces/pieces";
 export * from "./is_on_board";
-export * from "./has_piece";
-export * from "./get_piece";
+export * from "./get_turn";
+export * from "./coords_match";
